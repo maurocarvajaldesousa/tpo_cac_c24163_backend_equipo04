fa2a38996163836ddde42874b391b6c36d9b7db9
maurocds <mauro.carvajaldesousa@gmail.com>
2024-06-19 09:55:56 -0300

    Agrego métodos en models.py: get_by_id, save, delete. Agrego endpoints en views.py: para manejar GET, POST, PUT y DELETE

--- app/models.py
+++ app/models.py
@@ -1,7 +1,6 @@
 from app.database import get_db
 
 class Obra:
-    # Constructor
     def __init__(self, id_obra=None, descripcion=None, anio=None, partido=None, localidad=None, latitud=None, longitud=None, gmaps=None, imagen=None, ruta=None, categoria=None, estado=None):
         self.id_obra = id_obra
         self.descripcion = descripcion
@@ -15,7 +14,7 @@ class Obra:
         self.ruta = ruta
         self.categoria = categoria
         self.estado = estado
-    
+
     def serialize(self):
         return {
             'id_obra': self.id_obra,
@@ -31,17 +30,49 @@ class Obra:
             'categoria': self.categoria,
             'estado': self.estado
         }
-        
+
     @staticmethod
     def get_all():
-        # Logica de buscar en la base todas las obras
         db = get_db()
         cursor = db.cursor()
         query = "SELECT * FROM obras"
         cursor.execute(query)
-        # Obtengo resultados
         rows = cursor.fetchall()
         obras = [Obra(id_obra=row[0], descripcion=row[1], anio=row[2], partido=row[3], localidad=row[4], latitud=row[5], longitud=row[6], gmaps=row[7], imagen=row[8], ruta=row[9], categoria=row[10], estado=row[11]) for row in rows]
-        # Cerramos el cursor
         cursor.close()
         return obras
+
+    @staticmethod
+    def get_by_id(id_obra):
+        db = get_db()
+        cursor = db.cursor()
+        query = "SELECT * FROM obras WHERE id_obra = %s"
+        cursor.execute(query, (id_obra,))
+        row = cursor.fetchone()
+        cursor.close()
+        if row:
+            return Obra(id_obra=row[0], descripcion=row[1], anio=row[2], partido=row[3], localidad=row[4], latitud=row[5], longitud=row[6], gmaps=row[7], imagen=row[8], ruta=row[9], categoria=row[10], estado=row[11])
+        return None
+
+    def save(self):
+        db = get_db()
+        cursor = db.cursor()
+        if self.id_obra is None:
+            query = ("INSERT INTO obras (descripcion, anio, partido, localidad, latitud, longitud, gmaps, imagen, ruta, categoria, estado) "
+                     "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
+            cursor.execute(query, (self.descripcion, self.anio, self.partido, self.localidad, self.latitud, self.longitud, self.gmaps, self.imagen, self.ruta, self.categoria, self.estado))
+            self.id_obra = cursor.lastrowid
+        else:
+            query = ("UPDATE obras SET descripcion=%s, anio=%s, partido=%s, localidad=%s, latitud=%s, longitud=%s, gmaps=%s, imagen=%s, ruta=%s, categoria=%s, estado=%s "
+                     "WHERE id_obra=%s")
+            cursor.execute(query, (self.descripcion, self.anio, self.partido, self.localidad, self.latitud, self.longitud, self.gmaps, self.imagen, self.ruta, self.categoria, self.estado, self.id_obra))
+        db.commit()
+        cursor.close()
+
+    def delete(self):
+        db = get_db()
+        cursor = db.cursor()
+        query = "DELETE FROM obras WHERE id_obra = %s"
+        cursor.execute(query, (self.id_obra,))
+        db.commit()
+        cursor.close()
